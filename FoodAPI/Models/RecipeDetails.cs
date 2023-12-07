91b1cf72f01651d57d33db9c0852655d7ae62d27
veerabharath123 <veerab1029@gmail.com>
2023-12-07 11:48:50 +0530

    Add project files.

---
+++ FoodAPI/Models/RecipeDetails.cs
@@ -0,0 +1,13 @@
+﻿namespace FoodAPI.Models
+{
+    public class RecipeDetails:Recipe
+    {
+        public int TotalIngredients { get; set; }
+        public List<Ingredient> Ingredients { get; set; }
+
+        public RecipeDetails()
+        {
+            Ingredients = new List<Ingredient>();
+        }
+    }
+}
