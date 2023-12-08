1fe7903d56beaee3ae7e3b176b1719f0d8747997
veerabharath123 <veerab1029@gmail.com>
2023-12-08 12:39:10 +0530

    Update image

--- FoodAPI/Repositories/DocumentRepository.cs
+++ FoodAPI/Repositories/DocumentRepository.cs
@@ -9,21 +9,23 @@ namespace FoodAPI.Repositories
     public class DocumentRepository: IDocumentRepository
     {
         private readonly FoodDbContext _context;
+        private readonly DateTime dt;
         public DocumentRepository(FoodDbContext context)
         {
+            dt = DateTime.Now;
             _context = context;
         }
 
         public async Task<Guid?> SaveImage(ImageDetails ImageDetails)
         {
-            var dt = DateTime.Now;
             if (!string.IsNullOrEmpty(ImageDetails.Base64))
             {
+                if (ImageDetails.Temp_Id != null) return await UpdateImage(ImageDetails);
                 var img = new Image
                 {
                     CREATED_DATE = dt.Date,
                     CREATED_TIME = TimeSpan.Parse(dt.ToString("HH:mm:ss")),
-                    IMAGE_DATA = Convert.FromBase64String(ImageDetails.Base64.Replace("data:image/jpeg;base64,","")),
+                    IMAGE_DATA = Convert.FromBase64String(ImageDetails.Base64),
                     IMAGE_NAME = ImageDetails.Image_Name,
                     IMAGE_TYPE = ImageDetails.Image_Type,
                     TEMP_ID = Guid.NewGuid(),
@@ -31,6 +33,22 @@ namespace FoodAPI.Repositories
                 await _context.Images.AddAsync(img);
                 if (await _context.SaveChangesAsync() > 0)
                     return img.TEMP_ID;
+            }
+            return null;
+        }
+        public async Task<Guid?> UpdateImage(ImageDetails ImageDetails)
+        {
+            var exist = await _context.Images.AsNoTracking().FirstOrDefaultAsync(x => x.TEMP_ID == ImageDetails.Temp_Id);
+            if(exist != null)
+            {
+                exist.IMAGE_DATA = Convert.FromBase64String(ImageDetails.Base64!);
+                exist.IMAGE_NAME = ImageDetails.Image_Name;
+                exist.IMAGE_TYPE = ImageDetails.Image_Type;
+                exist.UPDATED_DATE = dt.Date;
+                exist.UPDATED_TIME = TimeSpan.Parse(dt.ToString("HH:mm:ss"));
+                _context.Images.Update(exist);
+                if (await _context.SaveChangesAsync() > 0)
+                    return exist.TEMP_ID;
             }
             return null;
         }
