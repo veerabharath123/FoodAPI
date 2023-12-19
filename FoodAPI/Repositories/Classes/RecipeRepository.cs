fd1e5549720caf78445c9f51cea3c58ad5c7eb39
veerabharath123 <veerab1029@gmail.com>
2023-12-19 12:39:43 +0530

    experi group by

--- FoodAPI/Repositories/Classes/RecipeRepository.cs
+++ FoodAPI/Repositories/Classes/RecipeRepository.cs
@@ -11,17 +11,27 @@ namespace FoodAPI.Repositories
 {
     public class RecipeRepository: IRecipeRepository
     {
+        private readonly DateTime dt;
         private readonly FoodDbContext _context;
         private IDocumentRepository _documentRepository;
         public RecipeRepository(FoodDbContext context, IDocumentRepository documentRepository)
         {
+            dt = DateTime.Now;
             _context = context;
             _documentRepository = documentRepository;
         }
 
         public async Task<RecipeDetails> GetActiveRecipeById(decimal id)
         {
-
+            var result = (from a in _context.Recipes
+                          join b in _context.Ingredients on a.ID equals b.RECIPE_ID into ab
+                          where a.ACTIVE == "Y" && a.DELETE_FLAG != "Y" && a.ID == id
+                          group ab by a into g
+                          select new RecipeDetails()
+                          {
+                              Ingredients = g.SelectMany(x => x).Where(x => x.ACTIVE == "Y" && x.DELETE_FLAG != "Y").ToList(),
+                              RECIPE_NAME = g.Key.RECIPE_NAME,
+                          }).FirstOrDefault();
             var RecipeDetail = new RecipeDetails();
             var data = await _context.Recipes.AsNoTracking().FirstOrDefaultAsync(a => a.ACTIVE == "Y" && a.DELETE_FLAG != "Y"
                               && a.ID == id);
@@ -60,25 +70,27 @@ namespace FoodAPI.Repositories
         }
         public async Task<PagerResponse<RecipeDetails>> GetAllFavouriteRecipes(PagerRequest request)
         {
-            var RecipeDetails = new List<RecipeDetails>();
-            var data = await _context.Recipes.AsNoTracking().Where(a => a.ACTIVE == "Y" && a.DELETE_FLAG != "Y" && a.FAVOURITES == "Y").ToListAsync();
-            foreach (var item in data)
-            {
-                var RecipeDetail = new RecipeDetails();
-                RecipeDetail = JsonConvert.DeserializeObject<RecipeDetails>(JsonConvert.SerializeObject(item));
-                RecipeDetail.TotalIngredients = await _context.Ingredients.AsNoTracking().CountAsync(a => a.ACTIVE == "Y" && a.DELETE_FLAG != "Y"
-                              && a.RECIPE_ID == item.ID);
-                if (item.IMAGE_ID.HasValue)
-                {
-                    RecipeDetail.Image = await _documentRepository.GetImage(item.IMAGE_ID);
-                }
-                RecipeDetails.Add(RecipeDetail);
-            }
-            return await Pager<RecipeDetails>.Paginate(RecipeDetails, request);
+            var data = (from a in _context.Recipes
+                        join b in _context.Ingredients on a.ID equals b.RECIPE_ID into ingredientsGroup
+                        from ingobj in ingredientsGroup.DefaultIfEmpty()
+                        where a.DELETE_FLAG != "Y" && a.ACTIVE == "Y" && a.FAVOURITES == "Y"
+                        group a by new { a.ID, a.RECIPE_NAME, a.IMAGE_ID, a.RECIPE_TYPE_ID, a.FAVOURITES } into grouped
+                        select new RecipeDetails
+                        {
+                            TotalIngredients = grouped.Count(),
+                            RECIPE_NAME = grouped.Key.RECIPE_NAME,
+                            ID = grouped.Key.ID,
+                            IMAGE_ID = grouped.Key.IMAGE_ID,
+                            RECIPE_TYPE_ID = grouped.Key.RECIPE_TYPE_ID,
+                            FAVOURITES = grouped.Key.FAVOURITES
+                        }).AsNoTracking();
+            var result = await Pager<RecipeDetails>.Paginate(data, request);
+            if (request.view == "block-view")
+                result.Pages.Where(x => x.IMAGE_ID.HasValue).ToList().ForEach(async x => x.Image = await _documentRepository.GetImage(x.IMAGE_ID));
+            return result;
         }
         public async Task<decimal> AddRecipe(FoodAPI.Models.Recipe recipe)
         {
-            var dt = DateTime.Now;
             recipe.ACTIVE = "Y";
             recipe.DELETE_FLAG = "N";
             recipe.CREATED_DATE = dt.Date;
@@ -93,7 +105,6 @@ namespace FoodAPI.Repositories
             var exist = await _context.Recipes.AsNoTracking().FirstOrDefaultAsync(x => x.ID == recipe.ID && x.ACTIVE == "Y" && x.DELETE_FLAG != "Y");
             if(exist != null)
             {
-                var dt = DateTime.Now;
                 exist.DESCRIPTION = recipe.DESCRIPTION;
                 exist.RECIPE_NAME = recipe.RECIPE_NAME;
                 exist.IMAGE_ID = recipe.IMAGE_ID;
@@ -111,7 +122,6 @@ namespace FoodAPI.Repositories
             var exist = await _context.Recipes.AsNoTracking().FirstOrDefaultAsync(x => x.ID == id && x.ACTIVE == "Y" && x.DELETE_FLAG != "Y");
             if (exist != null)
             {
-                var dt = DateTime.Now;
                 exist.DELETE_FLAG = "Y";
                 exist.UPDATED_DATE = dt.Date;
                 exist.UPDATED_TIME = TimeSpan.Parse(dt.ToString("HH:mm:ss"));
@@ -122,7 +132,6 @@ namespace FoodAPI.Repositories
         }
         public async Task<decimal> AddIngredient(Ingredient ingredient)
         {
-            var dt = DateTime.Now;
             ingredient.ACTIVE = "Y";
             ingredient.DELETE_FLAG = "N";
             ingredient.CREATED_DATE = dt.Date;
@@ -138,7 +147,6 @@ namespace FoodAPI.Repositories
                         && x.ID == ingredient.ID && x.ACTIVE == "Y" && x.DELETE_FLAG != "Y");
             if(exist != null)
             {
-                var dt = DateTime.Now;
                 exist.UPDATED_DATE = dt.Date;
                 exist.UPDATED_TIME = TimeSpan.Parse(dt.ToString("HH:mm:ss"));
                 exist.INGREDIENT_NAME = ingredient.INGREDIENT_NAME;
@@ -164,7 +172,6 @@ namespace FoodAPI.Repositories
             var exist = await _context.Ingredients.AsNoTracking().FirstOrDefaultAsync(x => x.ID == id && x.ACTIVE == "Y" && x.DELETE_FLAG != "Y");
             if(exist != null)
             {
-                var dt = DateTime.Now;
                 exist.UPDATED_DATE = dt.Date;
                 exist.UPDATED_TIME = TimeSpan.Parse(dt.ToString("HH:mm:ss"));
                 exist.DELETE_FLAG = "Y";
